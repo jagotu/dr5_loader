4db46e25511280def882bbdabbe530a986e701d8
JaGoTu <jagotu@gmail.com>
2020-07-23 15:22:15 +0200

    load_map: small update

--- load_map.py
+++ load_map.py
@@ -1,45 +1,46 @@
+import os
 import idaapi
-from idc import *
 
-filename = AskFile(0, "", "Select MAP file")
-if filename is None or not os.path.isfile(filename):
-    exit()
+filename = idaapi.ask_file(0, "", "FILTER *.MAP\nSelect MAP file")
+if filename is not None and os.path.isfile(filename):
 
-real_base = {}
-map_base = {}
+    real_base = {}
+    map_base = {}
 
-real_base['DGROUP'] = idaapi.get_segm_base(idaapi.get_segm_by_name("DATA"))
-real_base['IGROUP'] = idaapi.get_segm_base(idaapi.get_segm_by_name("TEXT"))
+    real_base['DGROUP'] = idaapi.get_segm_base(idaapi.get_segm_by_name("DATA"))
+    real_base['IGROUP'] = idaapi.get_segm_base(idaapi.get_segm_by_name("TEXT"))
 
-with open(filename, "r") as f:
-    data = f.read().split('\n')
-    
-insection = None
+    with open(filename, "r") as f:
+        data = f.read().split('\n')
+        
+    insection = None
 
-for line in data:
-    if line.strip() == 'Origin   Group':
-        insection = "groups"
-    elif line.strip() == 'Address         Publics by Value':
-        insection = "publics_"
-    elif line.strip() == '' and insection == "publics_":
-        insection = "publics"
-    elif line.strip() == '':
-        insection = None
-    else:
-        if insection == "groups":
-            (addr, name) = line.strip().split('   ')
-            addr = addr.split(':')[0]
-            map_base[addr] = name
-        elif insection == "publics":
-            (addr, name) = line.strip().split('       ')
-            (base, off) = addr.split(':')
-            if not base in map_base:
-                print("Unkown section with base " + base)
-                continue
-            section_name = map_base[base]
-            if(section_name in real_base):
-                real_addr = real_base[section_name] + int(off, 16)
-                idaapi.add_entry(real_addr, real_addr, name, 1)
-            
+    for line in data:
+        if line.strip() == 'Origin   Group':
+            insection = "groups"
+        elif line.strip() == 'Address         Publics by Value':
+            insection = "publics_"
+        elif line.strip() == '' and insection == "publics_":
+            insection = "publics"
+        elif line.strip() == '':
+            insection = None
+        else:
+            if insection == "groups":
+                (addr, name) = line.strip().split('   ')
+                addr = addr.split(':')[0]
+                map_base[addr] = name
+            elif insection == "publics":
+                (addr, name) = line.strip().split('       ')
+                (base, off) = addr.split(':')
+                if not base in map_base:
+                    #print("Unkown section with base " + base)
+                    section_name = 'IGROUP' #pushpull hack
+                else:
+                    section_name = map_base[base]
+                
+                if(section_name in real_base):
+                    real_addr = real_base[section_name] + int(off, 16)
+                    idaapi.add_entry(real_addr, real_addr, name, 1)
+                
             
         
